0466f7fdaebd4afe2fa83fd66389baa5cc4cb0fa
Selena <davidalbertosanchez@gmail.com>
2019-02-08 09:24:21 -0400

    States, Group  Controller and Voting Model

---
+++ Democracy/Migrations/201902081150503_20190208.cs
@@ -0,0 +1,16 @@
+namespace Democracy.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _20190208 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Democracy/Models/State.cs
+++ Democracy/Models/State.cs
@@ -11,6 +11,8 @@ namespace Democracy.Models
         [Key]
         public int StateId { get; set; }
 
-        public int Description { get; set; }
+        [Required(ErrorMessage ="The field {0} is required")]
+        [StringLength(50,ErrorMessage ="The field {0} can contain maximum {1} and minimum {2} characters",MinimumLength =3)]
+        public string Description { get; set; }
     }
 }
